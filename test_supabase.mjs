1347ed026676b3e7dcf257d50632abcec01b314d
xutingjiang1004 <3347606286@qq.com>
2026-06-23 14:22:56 +0800

    增加mcp

--- test_supabase.mjs
+++ test_supabase.mjs
@@ -1,5 +1,5 @@
 const headers = {
-  'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0aG93eHFpZ25saGt3YXlrZ2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxNzE1MTEsImV4cCI6MjA5Mjc0NzUxMX0.fNmh0HjNuIZaJTa56gMITwKpJMQfJ8mBN41HMhvyDDA'
+  'apikey': 'sb_publishable_RFqxlLe3nKnbs_PUHrJjpg_AUVaOGbu'
 };
 const base = 'https://ithowxqignlhkwaykglt.supabase.co/rest/v1';
 
